1707f457174dafd779fd2d5c0737105f9a459e01
Avi Vahl <avi@vahl.co.il>
2024-08-17 10:55:13 +0300

    infra: re-enable lint for js/mjs/cjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,31 @@
 import pluginJs from "@eslint/js";
 import pluginTypescript from "typescript-eslint";
 
+for (const config of pluginTypescript.configs.recommendedTypeChecked) {
+  config.files = ["**/*.{ts,tsx,mts,cts}"]; // ensure config only targets TypeScript files
+}
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist/", "**/*.{js,mjs,cjs}"] },
+  { ignores: ["dist/"] },
   pluginJs.configs.recommended,
-
-  ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-
   {
     rules: {
       "no-console": "error",
+      "no-undef": "off",
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    },
+  },
+  ...pluginTypescript.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true } } },
+  {
+    files: ["**/*.{ts,tsx,mts,cts}"],
+    rules: {
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
     },
   },
-
   {
-    files: ["**/*.test.{ts,tsx}"],
+    files: ["**/*.test.{ts,tsx,mts,cts}"],
     rules: {
       // native node test runner types for describe() and it() return a promise, so disable this rule in tests
       "@typescript-eslint/no-floating-promises": "off",
